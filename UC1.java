61f50b249d440fd851b1f90fffb2ce6320b2c2b6
jg8225-max <jg8225@srmist.edu.in>
2026-02-23 11:44:08 +0530

    UC1

---
+++ UC1.java
@@ -0,0 +1,13 @@
+
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of Java application
+     */
+    public static void main(String[] args) {
+
+        // Printing OOPS to console
+        System.out.println("OOPS");
+
+    }
+}
